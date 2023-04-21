f6dd831cfce026e696d19f444b0117fd45905d18
localuser <localuser@VEA.LV>
2023-04-21 14:12:42 +0300

    New: added arraylist of 3 products

--- src/main/java/lv/venta/contoller/FirstController.java
+++ src/main/java/lv/venta/contoller/FirstController.java
@@ -1,5 +1,8 @@
 package lv.venta.contoller;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
 
-	
+	private ArrayList<Product> products = new ArrayList<>(List.of(
+			new Product("Orange", "fresh", 3.4f, 10), 
+			new Product("Kiwi", "green", 5.5f, 3), 
+			new Product("Pear", "yellow", 8.3f, 7)));
+
 	@GetMapping("/hello") //localhost:8080/hello
 	public String getHelloFunc() {
 		System.out.println("Sveiki!");
@@ -25,6 +32,15 @@ public class FirstController {
 	public String getOneProductFunc(Model model) {
 		Product prod = new Product("Apple", "tasty", 1.2f, 9);
 		model.addAttribute("packet", prod);
-		return "product-page";
+		return "one-product"; 	//will show one-product.html
+	}
+	
+	//TODO create an ArrayList with 3 products
+	@GetMapping("/three-products")	//localhost:8080/three-products
+	public String getThreeProductsFunc(Model model) {
+		model.addAttribute("packet", products);
+		return "three-products";		//will show products-page.html
+		
+		
 	}
 }
