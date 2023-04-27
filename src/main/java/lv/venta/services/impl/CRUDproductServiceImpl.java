f01102b81d63a3eb9f0de0d887d5a3621c38ad49
localuser <localuser@VEA.LV>
2023-04-27 11:44:11 +0300

    New: added function implementation

---
+++ src/main/java/lv/venta/services/impl/CRUDproductServiceImpl.java
@@ -0,0 +1,76 @@
+package lv.venta.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lv.venta.contoller.Product;
+import lv.venta.services.ICRUDProductService;
+
+@Service
+public class CRUDproductServiceImpl implements ICRUDProductService{
+
+	private ArrayList<Product> products = new ArrayList<>(List.of(
+			new Product("Orange", "fresh", 3.4f, 10), 
+			new Product("Kiwi", "green", 5.5f, 3), 
+			new Product("Pear", "yellow", 8.3f, 7)));
+	
+	@Override
+	public void addNewProduct(String title, String description, float price, int quantity) throws Exception {
+		//TODO verification with regex title and description
+		if(title != null && description != null && price > 0 && price < 10000 && quantity > 0 && quantity < 10000) {
+			boolean isFound = false;
+			for(Product temp : products) {
+				if(temp.getTitle().equals(title) && temp.getDescription().equals(description) && temp.getPrice() == price) {
+					temp.setQuantity(temp.getQuantity() + quantity);
+					isFound = true;
+					break;
+				}
+			}
+			if(!isFound) {
+				Product newProduct = new Product(title, description, price, quantity);
+				products.add(newProduct);
+			}
+		}
+		else {
+			throw (new Exception("Wrong parameters"));
+		}
+		
+	}
+
+	@Override
+	public ArrayList<Product> retrieveAllProducts() {
+		return products;	//vienkarsi atgriezam visu listu ar produktiem
+	}
+
+	@Override
+	public Product retrieveProductById(long id) throws Exception {
+		if(id > 0) {
+			for(Product temp : products) {
+				if(temp.getId() == id) {
+					return temp;
+				}
+			}
+			throw (new Exception("There is not product with this ID"));
+		}
+		else {
+			throw (new Exception("ID need to be positive"));
+		}
+	}
+
+	@Override
+	public void updateById(long id, String title, String description, float price, int quantity) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteProductById(long id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
