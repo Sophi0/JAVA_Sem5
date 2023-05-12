e74b8bc6a5d0be363a6fad8f2eb34b5a5689d50e
localuser <localuser@VEA.LV>
2023-05-12 15:13:13 +0300

    Add: add repo for Product

--- src/main/java/lv/venta/repos/IProductRepo.java
+++ src/main/java/lv/venta/repos/IProductRepo.java
@@ -1,5 +1,9 @@
 package lv.venta.repos;
 
-public interface IProductRepo {
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.contoller.Product;
+
+public interface IProductRepo extends CrudRepository<Product, Long> {	//jo long ir datu tips ID, bet jabut referencu tips, tapec no liela burta
 
 }
