9f5d026950b1ffa30e4cfafbc03c197fff832b34
localuser <localuser@VEA.LV>
2023-05-12 15:00:46 +0300

    Add: add jpa in Product model

--- src/main/java/lv/venta/contoller/Customer.java
+++
@@ -1,5 +0,0 @@
-package lv.venta.contoller;
-
-public class Customer {
-
-}
--- src/main/java/lv/venta/contoller/Product.java
+++ src/main/java/lv/venta/contoller/Product.java
@@ -1,13 +1,22 @@
 package lv.venta.contoller;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@Table(name = "product_table")	//this will be a table in DB; mes rakstam table galvena klases nosaukuma
+@Entity
 public class Product {
 	
+	@Column(name = "Title") //H2 -> TITlE, Mysql -> title
 	@NotNull
 	@Size(min = 3, max = 150)
 	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only latinletters and space")  	//only latin letters; + nozime neierobezoti daudz simbolus
@@ -15,23 +24,28 @@ public class Product {
 	//piemeram, lai title nebutu null un uzstaditas min un max vertibas
 	private String title;
 	
-	
+	@Column(name = "description")
 	@NotNull
 	@Size(min = 5, max = 400)
 	@Pattern(regexp = "[A-Z]{1}[a-z0-9\\ ]+", message = "Only latinletters and space")
 	private String description;
 	
+	@Column(name = "price")
 	@Min(0)
 	@Max(10000)
 	private float price;
 	
+	@Column(name = "quantity")
 	@Min(0)
 	@Max(1000000)
 	private int quantity;
+	
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	private static long idCounter = 1;
 	
-	
+
 	public String getTitle() {
 		return title;
 	}
@@ -60,14 +74,9 @@ public class Product {
 		return id;
 	}
 	public void setId() {
-		this.id = idCounter++;
+		this.id = id;
 	}
-	public static long getIdCounter() {
-		return idCounter;
-	}
-	public static void setIdCounter(long idCounter) {
-		Product.idCounter = idCounter;
-	}
+
 	
 	public Product(String title, String description, float price, int quantity) {
 		setId();
