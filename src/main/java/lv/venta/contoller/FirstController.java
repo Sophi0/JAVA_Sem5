373b82f2f3461b327cd9448b2e0d144d604852e8
localuser <localuser@VEA.LV>
2023-05-12 13:38:12 +0300

    New: ain controller added validation

--- src/main/java/lv/venta/contoller/FirstController.java
+++ src/main/java/lv/venta/contoller/FirstController.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import lv.venta.services.ICRUDProductService;
 
 @Controller
@@ -83,8 +85,8 @@ public class FirstController {
 	}
 	
 	@PostMapping("/add-product")
-	public String postAddProductFunc(Product product) {	//product nosaukums jaasakrit ar nosaukumu, kurs atrodas html failaa; product with all parameters 
-		//TODO verify if this product already exists
+	public String postAddProductFunc(@Valid Product product, BindingResult result) {	//product nosaukums jaasakrit ar nosaukumu, kurs atrodas html failaa; product with all parameters 
+		if(!result.hasErrors()) {
 		try {
 			CRUDservice.addNewProduct(product.getTitle(), product.getDescription(), product.getPrice(), product.getQuantity());
 			return "redirect:/all-products";
@@ -93,6 +95,10 @@ public class FirstController {
 			return "redirect:/error";	
 		}
 	}
+		else {
+			return "add-product-page";
+		}
+}
 
 	@GetMapping("/update-product/{id}")	//localhost:8080/update-product/2
 	public String getUpdateProductFunc(@PathVariable("id") long id, Model model) {
