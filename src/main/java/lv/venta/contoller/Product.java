6ecd9a5faaedb68c6fa48efbfb5ea79ba527c3e3
unknown <localuser@vea.lv>
2023-05-12 13:31:36 +0300

    New: added validation in Model class

--- src/main/java/lv/venta/contoller/Product.java
+++ src/main/java/lv/venta/contoller/Product.java
@@ -1,9 +1,32 @@
 package lv.venta.contoller;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class Product {
+	
+	@NotNull
+	@Size(min = 3, max = 150)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+")  	//only latin letters; + nozime neierobezoti daudz simbolus
+	//katru parametru validejam
+	//piemeram, lai title nebutu null un uzstaditas min un max vertibas
 	private String title;
+	
+	
+	@NotNull
+	@Size(min = 5, max = 400)
+	@Pattern(regexp = "[A-Z]{1}[a-z0-9\\ ]+")
 	private String description;
+	
+	@Min(0)
+	@Max(10000)
 	private float price;
+	
+	@Min(0)
+	@Max(1000000)
 	private int quantity;
 	private long id;
 	private static long idCounter = 1;
